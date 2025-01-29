9119177b13e2bce0b8c74d6de98450cf022994be
Felhasznalo <mihokadam24@gmail.com>
2025-01-29 21:59:43 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /* konzol rajzolgató
+         Négyzet és félpiramis rajzoló
+         * * * * * *
+         * * * * * *
+         * * * * * *
+         * * * * * *
+         * * * * * *
+
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+        */
+
+
+    }
+}
