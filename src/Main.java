ed6fd1ff463a7122495e1db6f30de9f6089f90f2
Felhasznalo <mihokadam24@gmail.com>
2025-01-29 22:02:23 +0100

    Implemented spaghetti code

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,30 @@ public class Main {
          *
         */
 
+        // spagetti kódban megvalósítva
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
